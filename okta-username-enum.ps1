7d99a3256ff98653d5eecc3256864c2953a344ab
helmutye0 <86020593+helmutye0@users.noreply.github.com>
2023-10-24 10:13:49 -0400

    Update okta-username-enum.ps1

--- okta-username-enum.ps1
+++ okta-username-enum.ps1
@@ -6,7 +6,7 @@ this script utilizes okta to enumerate usernames/email addresses at a given tena
 
 to use it, create a file called user-file.txt filled with all the various usernames you wish to test, then adjust the $targetTenant variable to store the tenant code of your target (for instance, if the target's okta login is https://tenantcode.okta.com, the $targetTenant you would set would be tenantcode), then run it
 
-the script will output a json file with all the user's found to exist
+the script will output a file with all the user's found to exist
 
 note: if a username exists but is not authorized for remote login, okta will return HTTP 401 -- for this reason, I have set it up so that the username is echoed to screen before each request -- if there is a 401 response after a username, it means the username exists but is not authorized for remote login via okta
 
